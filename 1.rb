2f820880b0091118066a9b404a697337483612e9
Ruslan Kornienko <rk@anahoret.com>
2014-07-16 16:24:40 +0300

    So nice

--- 1.rb
+++ 1.rb
@@ -1,5 +1,4 @@
-a = 0
-sum = 0
+a, sum = 0, 0
 while a < 1000
   sum += a if a%3 == 0 || a%5 == 0
   a += 1
--- 2.rb
+++ 2.rb
@@ -1,11 +1,6 @@
-c = 1
-sum = 0
-a = 0
-b = 0 
+a, b, c, sum = 0, 0, 1, 0
 while c < 4000000
-  a = b
-  b = c
-  c = a + b
+  a, b, c = b, c, (a+b)
   sum += c if c%2 == 0
 end
 puts sum
